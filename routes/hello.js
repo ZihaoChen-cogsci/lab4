c55de9eee36998ac5600ab5017c6bcb526d3c10a
ZihaoChen-cogsci <z5chen@ucsd.edu>
2021-01-29 04:51:51 +0800

    Update hello.js

--- routes/hello.js
+++ routes/hello.js
@@ -2,9 +2,13 @@
 /*
  * GET home page.
  */
-
+ exports.view = function(req, res){
+   res.render('index', {
+   	'name': '',
+   });
+ };
 exports.view = function(req, res){
-  var nameToShow = req.params.ucserNam;
+  var nameToShow = req.params.userName;
   res.render('hello', {
     "name": nameToShow,
   });
